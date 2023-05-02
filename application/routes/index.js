74663d634198ae08704168e066fce87d986c1004
Aneesh <122716157+aneesh6214@users.noreply.github.com>
2023-05-02 16:18:18 -0700

    fixed css for handlebars
    made the hbs files not dependent on location of the css/js files. they are now passed through by index.js

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name: "Aneesh Kumar" });
+  res.render('index', { title: 'Home', name: "Aneesh Kumar", js: ["index.js"] });
 });
 
 router.get('/login', function (req, res) {
-  res.render('login');
+  res.render('login', { title: 'Login', css: ["../css/login.css"] });
 });
 
 router.get('/register', function (req, res) {
-  res.render('registration');
+  res.render('registration', { title: 'Register', css: ["../css/registration.css"], js: ["registration.js"] });
 });
 
 router.get('/postvideo', function (req, res) {
-  res.render('postvideo');
+  res.render('postvideo', { title: 'Post a Video', css: ["../css/postvideo.css"] });
 });
 
 router.get('/profile', function (req, res) {
-  res.render('profile');
+  res.render('profile', { title: 'Profile', css: ["../css/profile.css"] });
 });
 
 router.get('/viewpost/:id(\\d+)', function (req, res) {
-  res.render('viewpost');
+  res.render('viewpost', { title: 'View Post ${req.params.id}', css: ["../css/viewpost.css"] });
 });
 
 module.exports = router;
